4dc658cb778aa7cddd24dad7c5e2639c2a9a8a79
rodiont97@gmail.com <rodiont97@gmail.com>
2024-08-07 09:54:36 +0300

    _not_very_secure_ finished (полезно)

---
+++ Codewars/not_very_secure.py
@@ -0,0 +1,36 @@
+﻿#                                                               Проверка пароля на недопустимые символы
+
+
+# Мой вариант
+from string import ascii_letters as l
+
+
+d = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+p = True
+
+def alphanumeric(password):
+    for i in password:
+        if i in l or i in d:
+            p = True
+        else:
+            p = False
+            break
+    if len(password) > 0:
+        print(p)
+    else:
+        print('False')
+        
+# Вариант профи (вот я и узнал про isalnum)
+        
+def alphanumeric_1(string):
+    return string.isalnum()        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    pass
